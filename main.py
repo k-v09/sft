7b534907b478c57c4f6a5d2c4decf68a2356e9c5
Noah Sehman <noah.sehman@gmail.com>
2022-04-25 16:43:05 -0700

    six screens, fixed screen drawing mistakes

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-import math
 import pygame
 import random
 
 #game window
+winx = 0
+winy = 0
 pygame.init()
 WIDTH = 1000
 HEIGHT = 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Placeholder name')
+#(1000 - 40) / 3 = 320
 
 #color defs
 BLACK = (0, 0, 0)
@@ -39,15 +41,22 @@ class screen():
          screenColor = WHITE
       else:
          screenColor = GREY"""
-      pygame.draw.rect(WIN, WHITE, pygame.Rect(self.x1, self.y1, self.x2, self.y2), 2)
+      pygame.draw.rect(WIN, WHITE, pygame.Rect(self.x1, self.y1, self.wid, self.height), 2)
 
 def screen_scroll():
    pass
 
 #aspect ratio of screens should roughly be 16:9
-s1 = screen((60, 80), 640, 360)
+s1 = screen((10, 80), 320, 190)
+s2 = screen((10, 280), 320, 190)
+s3 = screen((340, 80), 320, 190)
+s4 = screen((340, 280), 320, 190) #390, 270
+s5 = screen((670, 80), 320, 190)
+s6 = screen((670, 280), 320, 190)
+
 #main func
 def main():
+   global winx, winy
 
    #main game loop
    run = True
@@ -60,9 +69,24 @@ def main():
 
          if event.type == pygame.QUIT:
             run = False
+         
+         """if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+               winx += 25
+            elif event.key == pygame.K_LEFT:
+               winx -= 25
+            elif event.key == pygame.K_UP:
+               winy -= 25
+            elif event.key == pygame.K_DOWN:
+               winy += 25"""
       
       #rectangle = surface, color, (x1, y1, x2, y2)
       s1.draw_screen()
+      s2.draw_screen()
+      s3.draw_screen()
+      s4.draw_screen()
+      s5.draw_screen()
+      s6.draw_screen()
 
       pygame.display.flip()
 
